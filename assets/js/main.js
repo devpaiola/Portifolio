7de362fa08d88484fe1c9319aceeed7bd007d78d
Fabricio Paiola <gualbertofabricio4@gmail.com>
2024-11-08 13:24:52 -0300

    fix: mechendo com resolução

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,92 +9,25 @@ initScrollReveal();
 typeWrite(document.querySelector(".typewriter"));
 
 hoverChangeExperience(
-  ".digitalhouse",
-  `Trabalhei como instrutor conteudista em frontend na
-  Digital House, o trabalho consistia em criar conteúdos para as
-  aulas de especialização frontend. Alguns temas dos conteúdos
-  realizados foram: React, Redux, TypeScript, Testes,
-  GraphQL, Next.js, MUI, React Hook Form e styled-components.`,
-  "Professor conteudista em Frontend",
-  "Digital House",
-  "Nov 2021 - Nov 2022 (1 ano)"
+  ".workmonitor",
+  `O usuário realiza testes de segurança em sistemas e aplicações para identificar vulnerabilidades e garantir que sejam corrigidas antes de serem exploradas. Utiliza técnicas automáticas e manuais em pentests, focando em falhas como SQL injection e XSS, e reporta detalhes técnicos com provas de conceito (PoCs) para o time de desenvolvimento. Colabora em programas de bug bounty, unindo incentivos financeiros com a melhoria de segurança, mantendo sempre uma comunicação ética e responsabilidade no trabalho..`,
+  "Assistênte de Suporte Técnico",
+  "WorkMonitor",
+  "Out 2024 - O momento"
 );
 
 hoverChangeExperience(
-  ".zuplae",
-  `Trabalhei como Social Media e Designer na Zuplae que é um escola
-  de programação, onde teve como foco as criações de conteúdos sobre
-  programação para a comunidade dev através das redes sociais.`,
-  "Social Media e UI Designer",
-  "Zuplae",
-  "Jan 2022 - Abril 2022 (4 meses)"
+  ".bughunter",
+  `O usuário realiza testes de segurança em sistemas e aplicações para identificar vulnerabilidades e garantir que sejam corrigidas antes de serem exploradas. Utiliza técnicas automáticas e manuais em pentests, focando em falhas como SQL injection e XSS, e reporta detalhes técnicos com provas de conceito (PoCs) para o time de desenvolvimento. Colabora em programas de bug bounty, unindo incentivos financeiros com a melhoria de segurança, mantendo sempre uma comunicação ética e responsabilidade no trabalho.`,
+  "Bug Bount",
+  "Bug Crownd",
+  "Dez 2023 - O momento"
 );
 
 hoverChangeExperience(
-  ".codigofontetv",
-  `Trabalhei como Social Media e Designer no Código Fonte TV, 
-  onde teve como foco as criações de conteúdos sobre programação 
-  para a comunidade dev através das redes sociais.`,
-  "Social Media e Designer",
-  "Código Fonte TV",
-  "Jun 2021 - Jan 2022 (8 meses)"
-);
-
-hoverChangeExperience(
-  ".contweb",
-  `Realizei o estágio na ContWeb, uma empresa de contabilidade. 
-  Onde pela primeira vez tive experiência com o React. Além do
-  desenvolvimento frontend criei os designs da nova plataforma da empresa.`,
-  "Developer frontend e UI Designer",
-  "ContWeb",
-  "Set 2021 - Nov 2021 (3 meses)"
-);
-
-hoverChangeDescription(
-  ".html",
-  "HTML é uma linguagem de marcação, onde marcamos os elementos para definir quais informações a página vai exibir."
-);
-hoverChangeDescription(
-  ".css",
-  "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas."
-);
-hoverChangeDescription(
-  ".js",
-  "JavaScript é uma linguagem de programação que permite a você implementar elementos dinâmicos em páginas web."
-);
-hoverChangeDescription(
-  ".sass",
-  "Sass é um pré-processador CSS que adiciona alguns recursos que não estão disponíveis de forma nativa."
-);
-hoverChangeDescription(
-  ".react",
-  "React é uma biblioteca JavaScript com foco em criar interfaces de usuário de forma componetizada."
-);
-hoverChangeDescription(
-  ".next",
-  "Next.js é uma estrutura da web que permite funcionalidades como renderização do lado do servidor e geração de sites estáticos para web baseados em React."
-);
-hoverChangeDescription(
-  ".styled",
-  "styled-components é uma biblioteca que utiliza o conceito de CSS-in-JS, ou seja, que nos permite escrever códigos CSS dentro do Javascript."
-);
-hoverChangeDescription(
-  ".tailwind",
-  "Tailwind CSS é um framework CSS que nos fornece classes utilitárias com o propósito de estilizar as páginas."
-);
-hoverChangeDescription(
-  ".typescript",
-  "TypeScript é um superset do JavaScript que incluir recursos que não estão presentes na linguagem de forma nativa, além de torná-la estática."
-);
-hoverChangeDescription(
-  ".radix",
-  "Radix é uma biblioteca que fornece componentes acessíveis e sem estilos para a criação de aplicação em React."
-);
-hoverChangeDescription(
-  ".cypress",
-  "Cypress é um framework para automação de testes end-to-end, onde atualmente usa a linguagem JavaScript."
-);
-hoverChangeDescription(
-  ".storybook",
-  "Storybook é uma ferramenta que tem como objetivo documentar e testar os componentes da aplicação."
+  ".concilig",
+  `Responsável pela implementação do usuário em nosso perfil 'SOl Agora' gerenciando tickets e chamados.`,
+  "Op. teleatendimento",
+  "Concilig",
+  "Jul 2024 - Out 2024"
 );
